7c4a7797a1eeff50ac98e3ac69782aa1e51a6685
Kumar Satyam <T0246565@gemalto.com>
2022-07-18 21:25:41 +0530

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,70 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Flex, Button, Box, Text } from '@chakra-ui/react'
+import { baseUrl, fetchApi } from '../utils/fetchApi'
+
+
+
+const Banner = ({purpose, title1, title2, desc1, desc2, buttonText, imageUrl, linkName}) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p="5">
+      <Text color="gray.500" fontSize="sm" fontWeight="medium">{purpose}</Text>
+      <Text fontSize="3xl" fontWeight="bold">{title1} <br /> {title2}</Text>
+      <Text fontSize="lg" paddingTop="3" paddingBottom="3"  color="gray.700">{desc1} <br /> {desc2}</Text>
+      <Button fontSize="xl">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
+  </Flex>
+)
+
+export default function Home({propertyForRent, propertyForSale}) {
+  console.log(propertyForRent, propertyForSale)
+  return (
+    <Box>
+      <Banner 
+      purpose="RENT A HOME"
+      title1="Rental Homes For"
+      title2="Everyone"
+      desc1="Explore From Apartments, builder floors, villas"
+      desc2="and more"
+      buttonText="Explore Renting"
+      linkName='/search?purpose=for-rent'
+      imageUrl='https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1513&q=80'
+      />
+      {/* <Flex flexWrap="wrap">
+
+      </Flex> */}
+
+      <Banner 
+      purpose="BUY A HOME"
+      title1="Find, Buy & Own Your"
+      title2="Dream Home"
+      desc1="Explore From Apartments, Homes, villas"
+      desc2="and more"
+      buttonText="Explore Buying"
+      linkName='/search?purpose=for-sale'
+      imageUrl='https://images.unsplash.com/photo-1503174971373-b1f69850bded?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1513&q=80'
+
+      // imageUrl='https://images.unsplash.com/photo-1605276374104-dee2a0ed3cd6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODZ8fGhvbWV8ZW58MHx8MHx8&auto=format&fit=crop&w=1513&q=60 '
+      />
+      <Flex flexWrap="wrap">
+        
+      </Flex>
+    </Box>
+  )
+}
+export async function getStaticProps(){
+  const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=5`)
+  const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=5`)
+
+  return {
+    props: {
+      propertyForRent: propertyForRent?.hits,
+      propertyForSale: propertyForSale?.hits,
+    }
+  }
+
+}
+
