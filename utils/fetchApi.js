dcc4c8e6fae47b0bcfe6b70de8bc0099398d2407
Anonsatyam <Satyamdeveloper98@gmail.com>
2022-07-22 02:33:22 +0530

    Removed API Key From JS File For Security Reason

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com'
 export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': '5228768583msh0643281c1edb30fp18113fjsnfb20b29f0ba0',
+            'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
         }
     }); 
